158620988c80b687bc197e0973ada0f73082575f
Alex Felix <85413825+afsilva3021@users.noreply.github.com>
2024-05-03 12:40:23 -0300

    Update logar.php
    Removendo dados antigos do sistema

--- Models/logar.php
+++ Models/logar.php
@@ -6,14 +6,13 @@
         header("Location: ../index.php");
     }
 
-
     include("../Controllers/config.php");
     
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
       $email = $_POST["email"];
-     $senha = $_POST["senha"];
+      $senha = $_POST["senha"];
 
-    // Verifica as credenciais no banco de dados
+   
     $query = "SELECT * FROM USR WHERE USR_EMAIL = :email AND USR_PWD = :senha";
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(':email', $email);
@@ -37,28 +36,6 @@
         echo "<script>location.href='../index.php';</script>";
     }
 }
-
-  
-//
-//    $sql = "SELECT * FROM USR 
-//            WHERE USR_EMAIL = '{$email}' 
-//            AND USR_PWD = '{$senha}'";
-//
-//
-//    $res = $conn->query($sql) or die($conn->error);
-//
-//$row = $res->fetch_object();
-//
-//$qtd = $res->num_rows;
-
-//if($qtd > 0) {;
-//    $_SESSION["email"] = $email;
-//    $_SESSION["USR_NOME"] = $row->USR_NOME;
-//    $_SESSION["USR_GRUPO"] = $row->USR_GRUPO;
-//    $_SESSION["USR_DEPART"] = $row->USR_DEPART;
-//    $_SESSION["USR_CARGO"] = $row->USR_CARGO;
-//    echo "<script>location.href='../Views/dashboard.php';</script>";
-//}else {
 //   
 //     
 //}
