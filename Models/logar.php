008c26a3614b022db8d4174fd95341522b35aa97
Alex Felix da Silva <alex.felix@grupomater.com.br>
2024-05-03 12:34:04 -0300

    Atualização sesões

--- Models/logar.php
+++ Models/logar.php
@@ -19,14 +19,18 @@
     $stmt->bindParam(':email', $email);
     $stmt->bindParam(':senha', $senha);
     $stmt->execute();
-
+    
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($result as $row){
+        $_SESSION["USR_NOME"] = $row['USR_NOME'];
+        $_SESSION["USR_GRUPO"] = $row['USR_GRUPO'];
+        $_SESSION["USR_DEPART"] = $row['USR_DEPART'];
+        $_SESSION["USR_CARGO"] = $row['USR_CARGO'];
+    }
+    
     if ($stmt->rowCount() > 0) {
         $_SESSION["email"] = $email;
-        $_SESSION["USR_NOME"] = $stmt->USR_NOME;
-        $_SESSION["USR_GRUPO"] = $stmt->USR_GRUPO;
-        $_SESSION["USR_DEPART"] = $stmt->USR_DEPART;
-        $_SESSION["USR_CARGO"] = $stmt->USR_CARGO;
-        header("Location: ../Views/dashboard.php");
+          header("Location: ../Views/dashboard.php");
         exit();
     } else {
         echo "<script>alert('Email ou Senha Incorretos');</script>";
